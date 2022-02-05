5a93f35058ca50c3c65ca4798c1d5c71213bcd5f
Jochen Fähnlein <jochen@faehnlein-ipsheim.de>
2022-02-05 14:01:44 +0100

    Generierung aktualisiert

--- add_symbol.py
+++ add_symbol.py
@@ -24,6 +24,33 @@ class ReadableDir(argparse.Action):
             raise argparse.ArgumentTypeError("readable_dir:{0} is not a readable dir".format(prospective_dir))
 
 
+class GpxExtensionCategory:
+    ns_short: str = None
+    ns: str = None
+
+    def __init__(self, ns_short: str, ns: str):
+        self.ns_short = ns_short
+        self.ns = ns
+
+    def create_localname_ns(self, localname: str) -> str:
+        return '%s:%s' % (self.ns_short, localname)
+
+
+categories: Dict[str, List[str]] = {
+    'geraetehaeuser':    ['Feuerwehr', 'Gerätehaus'],
+    'hydrant_oberflur':  ['Feuerwehr', 'Wasserentnahme', 'Hydrant', 'Oberflurhydrant'],
+    'hydrant_unterflur': ['Feuerwehr', 'Wasserentnahme', 'Hydrant', 'Unterflurhydrant'],
+    'loeschteich':       ['Feuerwehr', 'Wasserentnahme', 'Löschteich'],
+    'zisterne':          ['Feuerwehr', 'Wasserentnahme', 'Zisterne'],
+    'saugstelle':        ['Feuerwehr', 'Wasserentnahme', 'Saugstelle']
+}
+
+category_extensions: List[GpxExtensionCategory] = [
+    GpxExtensionCategory('gpxx', 'http://www.garmin.com/xmlschemas/GpxExtensions/v3'),
+    GpxExtensionCategory('wptx1', 'http://www.garmin.com/xmlschemas/WaypointExtension/v1')
+]
+
+
 def get_all_gpx_files(source_dir: str) -> List[Path]:
     path = Path(source_dir)
     if not path.exists():
@@ -71,10 +98,10 @@ def modify_gpx_file(gpx_files: Dict[Path, str]):
             with parse(gpx_file) as dom:
                 for wpt in dom.getElementsByTagName('wpt'):
                     add_sym_to_gpx(dom, wpt, symbol_name)
-                    add_category_to_gpx(dom, wpt)
+                    add_category_to_gpx(dom, wpt, gpx.stem)
 
                 with gpx.open(mode='w', encoding='UTF-8') as file_write:
-                    dom.writexml(file_write, indent='  ', addindent='  ', newl='\n')
+                    dom.writexml(file_write, indent='  ', addindent='  ', encoding='utf-8')
 
 
 def add_sym_to_gpx(dom: Document, wpt: Node, symbol_name: str):
@@ -85,76 +112,36 @@ def add_sym_to_gpx(dom: Document, wpt: Node, symbol_name: str):
     wpt.appendChild(sym_node)
 
 
-def add_category_to_gpx(dom: Document, wpt: Node):
+def add_category_to_gpx(dom: Document, wpt: Node, file_name: str):
     extensions_node: Element = (wpt.getElementsByTagName('extensions')[:1] or [None])[0]
 
     if not extensions_node:
         extensions_node = dom.createElement('extensions')
         wpt.appendChild(extensions_node)
 
-    # gpxx=http://www.garmin.com/xmlschemas/GpxExtensions/v3
-    gpx_ns: str = 'http://www.garmin.com/xmlschemas/GpxExtensions/v3'
-    gpx_ns_short: str = 'gpxx'
+    for category_extension in category_extensions:
+        gpxx_waypoint_extension_node: Element = (extensions_node.getElementsByTagNameNS(category_extension.ns, 'WaypointExtension')[:1] or [None])[0]
 
-    gpxx_waypoint_extension_node: Element = (extensions_node.getElementsByTagNameNS(gpx_ns, 'WaypointExtension')[:1] or [None])[0]
-    if not gpxx_waypoint_extension_node:
-        dom.documentElement.setAttributeNS('xmls', '%s:%s' % ('xmlns', gpx_ns_short), gpx_ns)
+        if not gpxx_waypoint_extension_node:
+            dom.documentElement.setAttributeNS('xmls', '%s:%s' % ('xmlns', category_extension.ns_short), category_extension.ns)
+            
+            gpxx_waypoint_extension_node = dom.createElementNS(category_extension.ns, '%s:%s' % (category_extension.ns_short, 'WaypointExtension'))
+            extensions_node.appendChild(gpxx_waypoint_extension_node)
+        else:
+            for child in gpxx_waypoint_extension_node.childNodes:
+                gpxx_waypoint_extension_node.removeChild(child)
 
-        gpxx_waypoint_extension_node = dom.createElementNS(gpx_ns, '%s:%s' % (gpx_ns_short, 'WaypointExtension'))
-        extensions_node.appendChild(gpxx_waypoint_extension_node)
-
-        display_mode_node: Element = dom.createElementNS(gpx_ns, '%s:%s' % (gpx_ns_short, 'DisplayMode'))
-        display_mode_content_node: Node = dom.createTextNode('SymbolOnly')
-        display_mode_node.appendChild(display_mode_content_node)
+        display_mode_node: Element = dom.createElementNS(category_extension.ns, '%s:%s' % (category_extension.ns_short, 'DisplayMode'))
+        display_mode_node.appendChild(dom.createTextNode('SymbolOnly'))
         gpxx_waypoint_extension_node.appendChild(display_mode_node)
 
-        categories_node: Element = dom.createElementNS(gpx_ns, '%s:%s' % (gpx_ns_short, 'Categories'))
+        categories_node: Element = dom.createElementNS(category_extension.ns, '%s:%s' % (category_extension.ns_short, 'Categories'))
         gpxx_waypoint_extension_node.appendChild(categories_node)
 
-        category1_node: Element = dom.createElementNS(gpx_ns, '%s:%s' % (gpx_ns_short, 'Category'))
-        category1_content_node: Node = dom.createTextNode('Feuerwehr')
-        category1_node.appendChild(category1_content_node)
-        categories_node.appendChild(category1_node)
-
-        # TODO: abhängig der Datei muss die Kategorie anders gesetzt werden
-        category2_node: Element = dom.createElementNS(gpx_ns, '%s:%s' % (gpx_ns_short, 'Category'))
-        category2_content_node: Node = dom.createTextNode('Feuerwehr Gerätehäuser')
-        category2_node.appendChild(category2_content_node)
-        categories_node.appendChild(category2_node)
-
-    # wptx1=http://www.garmin.com/xmlschemas/WaypointExtension/v1
-    garmin_ns: str = 'http://www.garmin.com/xmlschemas/WaypointExtension/v1'
-    garmin_ns_short: str = 'wptx1'
-
-    garmin_waypoint_extension_node: Element = (extensions_node.getElementsByTagNameNS(garmin_ns, 'WaypointExtension')[:1] or [None])[0]
-    if not garmin_waypoint_extension_node:
-        dom.documentElement.setAttributeNS('xmls', '%s:%s' % ('xmlns', garmin_ns_short), garmin_ns)
-
-        garmin_waypoint_extension_node = dom.createElementNS(garmin_ns, '%s:%s' % (garmin_ns_short, 'WaypointExtension'))
-        extensions_node.appendChild(garmin_waypoint_extension_node)
-
-        display_mode_node: Element = dom.createElementNS(garmin_ns, '%s:%s' % (garmin_ns_short, 'DisplayMode'))
-        display_mode_content_node: Node = dom.createTextNode('SymbolOnly')
-        display_mode_node.appendChild(display_mode_content_node)
-        garmin_waypoint_extension_node.appendChild(display_mode_node)
-
-        categories_node: Element = dom.createElementNS(garmin_ns, '%s:%s' % (garmin_ns_short, 'Categories'))
-        garmin_waypoint_extension_node.appendChild(categories_node)
-
-        category1_node: Element = dom.createElementNS(garmin_ns, '%s:%s' % (garmin_ns_short, 'Category'))
-        category1_content_node: Node = dom.createTextNode('Feuerwehr')
-        category1_node.appendChild(category1_content_node)
-        categories_node.appendChild(category1_node)
-
-        # TODO: abhängig der Datei muss die Kategorie anders gesetzt werden
-        category2_node: Element = dom.createElementNS(garmin_ns, '%s:%s' % (garmin_ns_short, 'Category'))
-        category2_content_node: Node = dom.createTextNode('Feuerwehr Gerätehäuser')
-        category2_node.appendChild(category2_content_node)
-        categories_node.appendChild(category2_node)
-
-
-    # getElementsByTagNameNS(namespaceURI, localName)
-    # createElementNS
+        for category_name in categories[file_name]:
+            category_node: Element = dom.createElementNS(category_extension.ns, '%s:%s' % (category_extension.ns_short, 'Category'))
+            category_node.appendChild(dom.createTextNode(category_name))
+            categories_node.appendChild(category_node)
 
 
 if __name__ == "__main__":
@@ -175,3 +162,5 @@ if __name__ == "__main__":
     found_gpx_files: List[Path] = get_all_gpx_files(Path(args.source))
     target_gpx_files: Dict[Path, str] = generate_target_structure(Path(args.target), found_gpx_files)
     modify_gpx_file(target_gpx_files)
+
+    sys.exit(0)
--- icons/README.md
+++ icons/README.md
@@ -2,6 +2,8 @@
 inkscape --export-type=png -w 128 -h 128 *.svg
 
 mogrify -format bmp -alpha set *.png
+unter Windows:
+magick mogrify -format bmp -alpha set *.png
 
 rm *.png
 
