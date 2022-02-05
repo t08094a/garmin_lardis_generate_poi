737cf99391360fa6d69c1fbe3a83cb4d60d4d7e3
Jochen Fähnlein <jochen@faehnlein-ipsheim.de>
2022-02-05 14:44:37 +0100

    Startdatei für Windows hinzugefügt

---
+++ generate.cmd
@@ -0,0 +1,8 @@
+python.exe .\add_symbol.py .\gpx .\generated_gpx
+
+"C:\Program Files (x86)\Garmin\POILoader\POILoader.exe" /Silent /Directory .\generated_gpx\geraetehaeuser
+"C:\Program Files (x86)\Garmin\POILoader\POILoader.exe" /Silent /Directory .\generated_gpx\hydrant_oberflur
+"C:\Program Files (x86)\Garmin\POILoader\POILoader.exe" /Silent /Directory .\generated_gpx\hydrant_unterflur
+"C:\Program Files (x86)\Garmin\POILoader\POILoader.exe" /Silent /Directory .\generated_gpx\loeschteich
+"C:\Program Files (x86)\Garmin\POILoader\POILoader.exe" /Silent /Directory .\generated_gpx\saugstelle
+"C:\Program Files (x86)\Garmin\POILoader\POILoader.exe" /Silent /Directory .\generated_gpx\zisterne
